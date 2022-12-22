1e71003e306834d26d47544cca0f78549eb68ee6
BigMoney031026 <bigmoney031026@gmail.com>
2022-12-22 14:15:36 -0600

    fix:ssl

--- index.js
+++ index.js
@@ -30,86 +30,9 @@ app.set('view engine', 'html');
 //Routes
 app.use('/', require('./routes/router'));
 app.use(express.static(path.normalize(__dirname + '/build')))
-// app.get('*', (req, res, next) => {
-//     console.log(req.protocol)
-//     if (req.protocol==='http') {
-//         console.log(req.protocol)
-//       return res.redirect('https://' + req.hostname + req.url)
-//     }
-//     next()
-// })
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"));
 })
 let server = http.createServer(app);
-let httpsServer = null
-const file_key = __dirname+'/certs/arbitrage.key';
-const file_crt = __dirname+'/certs/arbitrage.crt';
-if (fs.existsSync(file_key) && fs.existsSync(file_crt) ) { // && fs.existsSync(file_ca)
-    const key = fs.readFileSync(file_key, 'utf8')
-    const cert = fs.readFileSync(file_crt, 'utf8')
-    /* const caBundle = fs.readFileSync(file_ca, 'utf8')
-    const ca = caBundle.split('-----END CERTIFICATE-----\n') .map((cert) => cert +'-----END CERTIFICATE-----\n')
-    ca.pop() */
-    const options = {cert,key} // ,ca
-    httpsServer = https.createServer(options,app)
-    // initSocket(httpsServer)
-} else {
-    console.log("Do not find ssl files, disabled ssl features.")
-}
-setTimeout(function(){
-    try {
-        const query = `SELECT * FROM usdcreward`;
-            con.query(query, function (err, result1) {
-                if (err) throw err;
-                if (result1.length > 0) {
-                    for(var i=0;i<result1.length;i++){
-                        let amount = result1[i].rewardamount
-                        let address = result1[i].userwalletaddress
-                        console.log(amount,address,'USDC')
-                        const q = `SELECT * FROM depositusdc WHERE useraddress='${result1[i].userwalletaddress}' AND (status='2' OR status='3')`;
-                        con.query(q, function (err, result2) {
-                            for(var j=0;j<result2.length;j++){
-                                amount = amount + result2[j].amount/100
-                            }
-                                console.log(amount,'after USDC')
-                            const q1 = `UPDATE usdcreward SET rewardamount = '${amount}' WHERE userwalletaddress='${address}'`;
-                            con.query(q1, function (err, result3) {
-                                console.log(result3)
-                                if (err) throw err;
-                            })
-                        })
-                    }
-                }
-        })
-        const queryUsdt = `SELECT * FROM usdtreward`;
-        con.query(queryUsdt, function (err, result1) {
-            if (result1.length > 0) {
-                for(var i=0;i<result1.length;i++){
-                    let address = result1[i].userwalletaddress
-                    let amount = result1[i].rewardamount
-                    console.log(amount,address,'USDT')
-                    const q = `SELECT * FROM depositusdt WHERE useraddress='${result1[i].userwalletaddress}' AND (status='2' OR status='3')`;
-                    con.query(q, function (err, result2) {
-                        for(var j=0;j<result2.length;j++){
-                            amount = amount + result2[j].amount/100
-                        }
-                        console.log(amount,'after USDT')
-                        const q1 = `UPDATE usdtreward SET rewardamount = '${amount}' WHERE userwalletaddress='${address}'`;
-                        con.query(q1, function (err, result3) {
-                        })
-                    })
-                }
-            }
-        })
-        } catch (error) {
-            console.log(error)
-        }
-},3000)
-const portHttp = process.env.HTTP || 80;
-const portHttps = process.env.HTTPS || 443;
+const portHttp = process.env.HTTP || 6000;
 server.listen({ port: portHttp, host:'0.0.0.0' }, ()=>console.log(`Started http service on port ${portHttp}`))
-if (httpsServer) {
-    httpsServer.listen({ port: portHttps, host:'0.0.0.0' }, ()=>console.log(`Started HTTPS service on port ${portHttps}`))
-    
-}

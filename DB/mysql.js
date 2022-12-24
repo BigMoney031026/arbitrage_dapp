b04ede167413241c3a8ee8ce3d50859ed436f1f1
DeamChain CEO <103885931+leopawel@users.noreply.github.com>
2022-12-24 19:00:48 +0100

    fix: database password

--- DB/mysql.js
+++ DB/mysql.js
@@ -7,7 +7,7 @@ const mysql = require('mysql');
 let connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.PASSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME
 });
 // const util = require('util');
